6b11be0a2a8f7726ff15b07e60cc13ed7eda3cbc
Vshnv2001 <mvaishnav.sg@gmail.com>
2024-09-21 09:16:08 +0800

    Add Dynamic Integration

--- frontend/src/components/layout/header.tsx
+++ frontend/src/components/layout/header.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Link from 'next/link';
 import {
   Menu,
@@ -14,6 +14,12 @@ import { IconChevronDown, IconWallet, IconCopy } from '@tabler/icons-react';
 import { MantineLogo } from '@mantinex/mantine-logo';
 import classes from '../../styles/HeaderMenu.module.css';
 import { useWeb3 } from '../../contexts/web3context';
+import { EthereumWalletConnectors } from '@dynamic-labs/ethereum';
+import { DynamicContextProvider, DynamicEmbeddedWidget, DynamicWidget, getAuthToken, useDynamicContext } from '@dynamic-labs/sdk-react-core';
+import { DynamicWagmiConnector } from '@dynamic-labs/wagmi-connector';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { createConfig, http, WagmiProvider } from 'wagmi';
+import { mainnet } from 'viem/chains';
 
 const links = [
   { link: '/', label: 'Home' },
@@ -29,27 +35,22 @@ const links = [
   { link: '/about', label: 'About' },
 ];
 
+const config = createConfig({
+  chains: [mainnet],
+  multiInjectedProviderDiscovery: false,
+  transports: {
+    [mainnet.id]: http(),
+  },
+});
+
+const queryClient = new QueryClient();
+
 export function HeaderMenu() {
   const [opened, { toggle }] = useDisclosure(false);
   const { account, connectWallet, disconnectWallet } = useWeb3();
   const clipboard = useClipboard({ timeout: 1500 }); // Clipboard state with a 1.5-second timeout
-
-  const handleWalletClick = () => {
-    if (!account) {
-      connectWallet();
-    }
-  };
-
-  const handleDisconnect = () => {
-    disconnectWallet();
-  };
-
-  const handleCopy = () => {
-    if (account) {
-      clipboard.copy(account);
-    }
-  };
-
+  const [jwtToken, setJwtToken] = useState(null);
+  console.log(jwtToken);
   const items = links.map((link) => {
     const menuItems = link.links?.map((item) => (
       <Menu.Item key={item.link}>
@@ -92,65 +93,40 @@ export function HeaderMenu() {
   });
 
   return (
-    <header className={classes.header}>
-      <Container size="md">
-        <div className={classes.inner}>
-          <MantineLogo size={28} />
-          <Group gap={5} visibleFrom="sm" className={classes.navGroup}>
-            {items}
-          </Group>
-          <Group gap={5} visibleFrom="sm" className={classes.buttonGroup}>
-            {account ? (
-              <Menu
-                width={200}
-                trigger="hover" openDelay={100} closeDelay={400}
-                position="bottom-end"
-
-                withinPortal
-              >
-                <Menu.Target>
-                  <Button
-                    leftSection={<IconWallet size="1rem" />}
-                    variant="outline"
-                    onClick={handleWalletClick}
-                  >
-                    {`${account.slice(0, 6)}...${account.slice(-4)}`}
-                  </Button>
-                </Menu.Target>
-                <Menu.Dropdown>
-                  <Menu.Item
-                    leftSection={<IconCopy size="0.9rem" />}
-                    onClick={handleCopy}
-                  >
-                    <Tooltip
-                      label={clipboard.copied ? 'Copied!' : 'Copy Wallet ID'}
-                      withArrow
-                      position="right"
-                      opened={clipboard.copied} // Tooltip opens only when copied
-                    >
-                      <span style={{ display: 'block' }}>
-                        {clipboard.copied ? 'Copied!' : 'Copy Wallet ID'}
-                      </span>
-                    </Tooltip>
-                  </Menu.Item>
-                  <Menu.Item color="red" onClick={handleDisconnect}>
-                    Disconnect
-                  </Menu.Item>
-                </Menu.Dropdown>
-              </Menu>
-            ) : (
-              <Button
-                leftSection={<IconWallet size="1rem" />}
-                variant="outline"
-                onClick={handleWalletClick}
-              >
-                Connect Wallet
-              </Button>
-            )}
-          </Group>
-          <Burger opened={opened} onClick={toggle} size="sm" hiddenFrom="sm" />
-        </div>
-      </Container>
-    </header>
+    <DynamicContextProvider
+      settings={{
+        environmentId: '2c4aad80-975a-42d6-b20b-656b7cd82675',
+        walletConnectors: [EthereumWalletConnectors],
+      }}
+    >
+      <header className={classes.header}>
+        <Container size="md">
+          <div className={classes.inner}>
+            <MantineLogo size={28} />
+            <Group gap={5} visibleFrom="sm" className={classes.navGroup}>
+              {items}
+            </Group>
+            <Group gap={5} visibleFrom="sm" className={classes.buttonGroup}>
+              <WagmiProvider config={config}>
+                <QueryClientProvider client={queryClient}>
+                  <DynamicWagmiConnector>
+                    <DynamicWidget />
+                    <AccountInfo setJwtToken={setJwtToken} />
+                  </DynamicWagmiConnector>
+                </QueryClientProvider>
+              </WagmiProvider>
+            </Group>
+            <Burger opened={opened} onClick={toggle} size="sm" hiddenFrom="sm" />
+          </div>
+        </Container>
+      </header>
+    </DynamicContextProvider>
   );
 }
+
+function AccountInfo({setJwtToken}: {setJwtToken: any}) {
+  const { authToken, handleLogOut, user, setShowAuthFlow } = useDynamicContext();
+  const dynamicJwtToken = getAuthToken();
+  setJwtToken(dynamicJwtToken || '');
+  return null
+};
